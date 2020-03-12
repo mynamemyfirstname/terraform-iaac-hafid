969c99e48c5729fe5d0dd5183680bede566bedfd
hafid elhafa <hafid elhafa@gmail.com>
2020-03-12 14:02:13 -0500

    added 2

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "web" {
   key_name      = "${aws_key_pair.deployer.key_name}"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web2" {
   ami           = "ami-0fc61db8544a617ed"
   instance_type = "t2.micro"
   key_name      = "${aws_key_pair.deployer.key_name}"
