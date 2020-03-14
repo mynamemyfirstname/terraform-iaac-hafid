7d262d0214c244b45751fd9d4df1d2a1fe315f39
hafid elhafa <hafid elhafa@gmail.com>
2020-03-14 11:14:56 -0500

    added file

--- buckend.tf
+++ buckend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-ana"
-    key    = "path/to/my/key"
+    key    = "path/to/my/vpc"
     region = "us-east-1"
     dynamodb  = "terraform-class"
 }
